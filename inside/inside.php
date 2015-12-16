2b8955083c9dbd7646d3361629845267761aabf1
icchiy <icchi050502@gmail.com>
2015-12-16 15:24:02 +0800

    no message

---
+++ inside/inside.php
@@ -0,0 +1,244 @@
+<?php
+
+require('../dbconnect.php');
+
+//投稿を取得する。  
+  $sql=sprintf('SELECT article.title,article.content FROM article ORDER BY `created` DESC');
+  $posts=mysqli_query($db,$sql)or die (mysqli_error($db));
+//自作関数　htmlspecialcharsのショートカット
+  function h($value){
+  return htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+  }
+
+  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>AppSNS TOP</title>
+	<link rel="stylesheet" type="text/css" href="inside.css">
+		<script src="../jquery-2.1.4.min.js"></script>
+	<body>
+		<div class="main">
+			
+			<!-- サイドバー -->
+			<div class="sidebar">
+				<a href="../../AppSNS/profile/profile.html"><img src="../../AppSNS/image/sushi.jpg" class="profile-icon"></a>
+				<div class="profile">
+
+					<ul>
+						<li><h4>お寿司</h4></li>
+						<li><p>Apple JAPAN Inc. → 金融系IT</p></li>
+					</ul>
+
+					<div class="follow">
+						<div class="f-f">
+							<a href="#">
+								<div class="following">
+									<div class="follow-count"><h4>36</h4></div>
+									<div class="follow-caption"><p>フォロワー</p></div>
+								</div>
+							</a>
+
+							<a href="#">
+								<div class="follower">
+									<div class="follow-count"><h4>18</h4></div>
+									<div class="follow-caption"><p>フォロワー</p></div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+
+
+
+				<!-- カテゴリ -->
+				<div class="category">
+
+					<script>
+					$(function() {
+						var acc = $(".category-top");
+
+						$("> dd", acc).hide();
+
+						$("> dt", acc).addClass("active");
+
+						$("> dt", acc).hover(function(){
+							$(this).addClass("hover");
+						}, function(){
+							$(this).removeClass("hover");
+						})
+
+					.click(function(){
+						if( $(this).next("dd").css("display")=="none"){
+
+							$("> dd", acc).slideuo("fast");
+
+							$("> dt.active", acc).removeClass("active");
+
+							$(this).addClass("active").next("dd").slideDown("fast");
+						}
+						
+						})
+
+					});
+					</script>
+
+
+					<!-- カテゴリ選択 -->
+					<dl class="category-top">
+					<h4>カテゴリ</h4>
+
+						<dt class="game">
+							<p>　ゲーム</p>
+						</dt><!-- game -->
+							<dd><p>RPG</p></dd>
+							<dd><p>アクション</p></dd>
+							<dd><p>パズル</p></dd>
+							<dd><p>スポーツ</p></dd>
+
+						<dt class="map">
+							<p>　地図</p>
+						</dt><!-- traffic -->
+							<dd><p>MAP</p></dd>
+							<dd><p>乗換案内</p></dd>
+							<dd><p>グルメ</p></dd>
+							<dd><p>トラベル</p></dd>
+
+						<dt class="everyday">
+							<p>　日常</p>
+						</dt><!-- camera -->
+							<dd><p>カメラ</p></dd>
+							<dd></dd>
+							<dd></dd>
+							<dd></dd>
+
+						<dt class="study">
+							<p>　学習</p>
+						</dt><!-- study -->
+							<dd><p>語学</p></dd>
+							<dd><p>プログラミング</p></dd>
+							<dd><p>受験</p></dd>
+
+					</dl><!-- category-top -->
+
+				</div><!-- category -->
+					
+				
+			</div><!-- sidebar -->
+
+			<div class="kiji">
+				<div class="">
+					<div class="post">
+
+						<div class="post-top">
+								<img src="../image/IMG_6910.jpg">
+								<div class="top-content">
+								<h2>アプリのなまえ</h2>
+								<p>アプリアプリアプリアプリアプリアプリアプリアプリアプリ
+									アプリアプリアプリアプリアプリアプリアプリアプリアプリ
+								アプリアプリアプリアプリアプリアプリアプリアプリアプリ
+							アプリアプリアプリアプリアプリアプリアプリアプリアプリアプリアプリアプリ</p></div>
+						</div>
+<!-- ここから -->
+						  <div id="main">
+
+							<div id="gallery">
+
+							<div id="slides">
+
+							<div class="slide"><img src="../image/IMG_6910.jpg" /></div>
+							<div class="slide"><img src="../image/IMG_6914.jpg" /></div>
+							<div class="slide"><img src="../image/IMG_6915-1.jpg" /></div>
+							<div class="slide"><img src="../image/IMG_6915-2-1.jpg" /></div>
+							<div class="slide"><img src="../image/IMG_6917-1.jpg" /></div>
+							<div class="slide"><img src="../image/IMG_6928-1.jpg" /></div>
+							
+
+							</div>
+
+							<div id="menu" style="padding-bottom: 40px;">
+
+							<ul>
+							<li class="fbar">&nbsp;</li>
+							<li class="menuItem"><a href=""><img src="../image/IMG_6910.jpg" /></a></li>
+							<li class="menuItem"><a href=""><img src="../image/IMG_6914.jpg" /></a></li>
+							<li class="menuItem"><a href=""><img src="../image/IMG_6915-1.jpg" /></a></li>
+							<li class="menuItem"><a href=""><img src="../image/IMG_6915-2-1.jpg" /></a></li>
+							<li class="menuItem"><a href=""><img src="../image/IMG_6917-1.jpg" /></a></li>
+							<li class="menuItem"><a href=""><img src="../image/IMG_6928-1.jpg" /></a></li>
+
+							</ul>
+
+							</div>
+
+							</div>
+
+							</div>
+
+<!-- 							ここまで -->
+
+						  <h1>『ペコッター』登録不要で使える！3分でオススメの飲食店をクチコミで教えてくれるアプリが登場</h1>
+						  <p>
+						    こんにちは、SHANです。
+							そろそろ忘年会のシーズンということで、会場となるお店探しに手間取っている方もいらっしゃるのではないでしょうか。
+							そこで今回は、チャット上でいくつかの質問をするだけで自分に合ったお店をカンタンに探せてしまうアプリ『ペコッター』をご紹介します。
+							メールアドレスの登録や会員登録などの面倒な手順を踏まずに今すぐ利用することができちゃいますよ〜！</p><br>
+						    <br>
+
+						    <h3>チャット上でいくつか質問をするだけ！</h3><br>
+						    <img src="../image/IMG_6910.jpg"><br>
+						    <p>
+						    	『ペコッター』ではメールアドレスやSNSの連携などを必要とせず<br>
+								・ニックネーム<br>
+								・年齢<br>
+								・利用する地域<br>
+								・の項目を入力するだけで今スグ利用できるんです。<br>
+						    <p>
+
+
+						  
+
+						  <div class="comment">
+						    <h3>Aさん</h3>
+						    <p>
+						      記事１へのコメントです。<br>
+						      記事１へのコメントです。<br>
+						    </p>
+						  </div>
+						  <div class="comment">
+						    <h3>Bさん</h3>
+						    <p>
+						      記事１へのコメントです。
+						    </p>
+						  </div>
+						  <p class="commment_link">
+						    投稿日：2011/6/7
+						    <a href="#">コメント</a>
+						  </p>
+					</div>
+				</div>
+			</div><!-- kiji -->
+
+
+			<!-- ヘッダー -->
+			<header>
+				<div class="header-design">
+					<img src="../../AppSNS/image/logo2_white.png">
+				</div>
+			</header><!-- header -->
+
+
+
+		</div><!-- main -->
+	</body>
+</html>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/form.js"></script>
+
+
--- post/post.php
+++ post/post.php
@@ -1,30 +1,26 @@
+
 <?php
+
 require('../dbconnect.php');
+
 //  //投稿を記録する.※POST送信されたものがあるかどうか
 if(!empty($_POST)){
 //  メッセージが入力されていた時
-if($_POST['message']!=''){
+ if($_POST['title']||['content']!=''){
 //  //%d...整数型のデータを置換する文字
 //  //%s...文字型のデータを置換する文字
 //  //SQL文では数字をダブルクォーテーションで
-  $sql=sprintf('INSERT INTO Apple SET member_id=%d,title="%s",content="%s",created=NOW()',
-    mysqli_real_escape_string($db,$Apple['id']),
-    mysqli_real_escape_string($db,$_POST['title']),
-    mysqli_real_escape_string($db,$_POST['content'])
-    );
-    mysqli_query($db,$sql)or die (mysqli_error($db));
-    
-    header('Location:post.php');
-    exit();
-  }
+    $sql=sprintf('INSERT INTO article SET title="%s",content="%s",created=NOW()',
+        mysqli_real_escape_string($db,$_POST['title']),
+        mysqli_real_escape_string($db,$_POST['content'])
+        );
+        mysqli_query($db,$sql)or die (mysqli_error($db));
+        
+        header('Location:index.php');
+        exit();
+    }
 }
-//投稿を取得する。  
-  $sql=sprintf('SELECT article.ttitle,article.content FROM article ORDER BY `created` DESC');
-  $posts=mysqli_query($db,$sql)or die (mysqli_error($db));
-//自作関数　htmlspecialcharsのショートカット
-  function h($value){
-  return htmlspecialchars($value,ENT_QUOTES,'UTF-8');
-  }
+
 ?>
 
 <!DOCTYPE html>
@@ -72,17 +68,18 @@ if($_POST['message']!=''){
 
 <div class="zentai">
 
-    <form accept-charset="UTF-8" action="/lists/187234" class="edit_list" id="edit_list_187234" method="post" novalidate="novalidate" style="height:245px;"><div style="margin:0;padding:0;display:inline"><input type="hidden"><input name="_method" type="hidden" value="put"><input name="authenticity_token" type="hidden" value="jODrO4S+1+jVoV8dizU8pPml39gfdUQ20NBs516lIXE="></div>
-
+    <form accept-charset="UTF-8" action="inside.php" method="post">
+        <input name="_method" type="hidden" value="put">
 
          <div class="toko-menu">
             <div class="toko-menu-in">
-                <select class="toko-menu-categories" name="" style="width:198px;height:36px;">
-                  
+                <select class="toko-menu-categories" style="width:198px;height:36px;">
+                  <!-- 
                     <span class="ui-selectmenu-button ui-widget ui-state-default ui-corner-all" tabindex="0" id="list_big_category_id-button" role="combobox" aria-expanded="false" aria-autocomplete="list" aria-owns="list_big_category_id-menu" aria-haspopup="true" style="width: 200px;">
                     <span class="ui-icon ui-icon-triangle-1-s"></span>
-                    <span class="ui-selectmenu-text">カテゴリを選んでね</span>
-                </span>
+                     -->
+                <!-- </span> -->
+
                     <option value="">カテゴリを選択</option>
 
                     <option value="1">ファッション</option>
@@ -97,28 +94,28 @@ if($_POST['message']!=''){
                 </select>
 
                 <div class="toko-menu-edit">
-                <input class="edit-btn" data-disable-with="保存中..." name="commit" type="submit" value="公開する" style="background-color:#ff7f50;color:#fff;">
-                <input class="edit-btn" data-disable-with="保存中..." name="commit" type="submit" value="下書き保存">
+                <input class="edit-btn" data-disable-with="保存中..." name="commit" type="submit" value="投稿する" style="background-color:#ff7f50;color:#fff;">
+               <!--  <input class="edit-btn" data-disable-with="保存中..." name="commit" type="submit" value="下書き保存">
                 <input class="edit-btn" data-disable-with="保存中..." name="commit" type="submit" value="プレビュー">
-                <input class="edit-btn" data-disable-with="保存中..." name="commit" type="submit" value="記事作成">
+                <input class="edit-btn" data-disable-with="保存中..." name="commit" type="submit" value="記事作成"> -->
                 </div>
             </div><!-- toko-menu-in -->
         </div><!-- toko-menu -->
 
             <div class="toko-main">
-                <div class="toko-image"><img alt="タイトルを入力してください" height="160" id="edit_list_thumbnail" src="https://assets.mery.jp/1449638204321/images/noimage.png" width="160" data-pin-nopin="true">
+                <div class="toko-image"><img alt="タイトルを入力してください" height="160" src="https://assets.mery.jp/1449638204321/images/noimage.png" width="160" data-pin-nopin="true">
                 </div>
 
                  <div class="toko-text">
-                    <input id="toko-text-title" name="list[title]" placeholder="まとめのタイトル"  type="text">
+                    <input id="toko-text-title" name="title" placeholder="アプリの名前"  type="text" value="<?php echo $post['title']?>">
 
-                    <textarea id="toko-text-contents" name="list[description]" placeholder="まとめの説明（160文字以内）"><?php echo h($content);?></textarea>
+                    <textarea id="toko-text-contents" name="content" placeholder="アプリの説明（160文字以内）"><?php echo $post['content']?></textarea>
                     <p class="toko-text-p"><span class="count">0</span>/160文字</p>
                 </div>
             </div><!-- toko-main -->
         
 </form>
-        
+        <!-- 
 
         <div class="file-main">
 
@@ -131,101 +128,8 @@ if($_POST['message']!=''){
                 <input id="file-settei-input" data-confirm="著作権や第三者の権利を侵害する画像をアップロードした場合、利用規約および関連法規により処罰を受ける可能性があります。" data-disable-with="設定中..." name="commit" type="submit" value="設定">
                 </div>
             </form>
-        </div>
+        </div> -->
 
-
-
-        <div class="edit-main">
-            <div class="edit-out">
-                <div class="editMenu">
-                    <ul class="edit-tool">
-                     
-                          <li class="edit-tool-text">
-                          <a href="javascript:void(0);"><i class="fa fa-pencil"><p>テキスト</p></i></a>
-                      </li>
-
-                        <li class="edit-tool-image">
-                          <a data-fancybox-type="ajax"><i class="fa fa-camera"><p>カメラ</p></i></a>
-                        </li>
-
-                        <li class="edit-tool-quote">
-                          <a href="javascript:void(0);"><i class="fa fa-quote-left"><p>引用</p></i></a>
-                        </li>
-
-                        <li class="edit-tool-headline">
-                          <a href="javascript:void(0);" ><i class="fa fa-list-ol"><p>見出し</p></i></a>
-
-                        <li class="edit-tool-link">
-                          <a href="javascript:void(0);"><i class="fa fa-globe"><p>リンク</p></i></a>
-                        </li>
-
-                        <li class="edit-tool-twitter">
-                          <a data-fancybox-type="ajax"><i class="fa fa-twitter"><p>twitter</p></i></a>
-                        </li>
-
-                        <li class="edit-tool-product">
-                          <a data-fancybox-type="ajax"><i class="fa fa-shopping-cart"><p>商品</p></i></a>
-                        </li>
-
-                        <li class="edit-tool-spot">
-                          <a data-fancybox-type="ajax"><i class="fa fa-map-marker"><p>スポット</p></i></a>
-                        </li>
-
-                        <li class="edit-tool-movie">
-                          <a href="javascript:void(0);"><i class="fa fa-youtube-play"><p>YouTube</p></i></a>
-                        </li>
-                    </ul>
-                </div><!-- editMenu -->
-                <div id="item_form_loader" class="loading" style="display: none;">
-            <img src="https://assets.mery.jp/1449638204321/images/loading.8e2e8ecb.gif" alt="ローディング...">
-          </div>
-
-           <div class="new_item"></div>
-           <div id="list_content"><article id="col2_article" class="articleArea editItem_area">
-  <ul id="sortable" class="ui-sortable">あああああああああああああああああ
-  </ul>
-</article>
-
-<script>
-  if(typeof jQuery !== 'undefined') {
-    jQuery(function($) {
-      $('#sortable').sortable({
-        handle: '.sort_icon',
-        update: function(event, ui) {
-          $("#item_form_loader").show();
-          var new_order = $(this).sortable('toArray').toString();
-          new_order = new_order.replace(/list_content,/g,'');
-          new_order = new_order.split(",");
-          new_order = new_order.filter(Boolean);
-          $.post(
-            '/items/sort_order',
-            {
-              'new_order': new_order,
-              'list_id': 187234
-            }
-          );
-        }
-      });
-      // グローバル関数としたくないため、
-      // 暫定的にlist_contentと同じ処理を記載 see mery #pull/3307
-      $(".delete_item").click(function () {
-        if(confirm("本当に削除してよろしいですか？？")) {
-          $("#new_item").html("");
-          $("#item_form_loader").show();
-          return true;
-        } else {
-          return false;
-        }
-      });
-    });
-  }
-</script></div>
-
-
-
-
-            </div><!-- edit-out -->
-        </div><!-- edit-main -->
 
 </div><!-- zentai -->
 
