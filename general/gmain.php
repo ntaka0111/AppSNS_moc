88308f00f815c9f354ee93b8e80f1a070fb71c4d
onizawa0719 <onionioni.poni@icloud.com>
2015-12-17 13:23:10 +0800

    main

--- general/gmain.php
+++ general/gmain.php
@@ -1,3 +1,92 @@
+<?php
+	// var_dump($_POST);
+	// $error = array();
+	require('../dbconnect.php');
+
+	session_start();
+	//ボタンが送信されてPOST送信されたら
+	if (!empty($_POST)) 
+	{
+		//エラー項目の確認
+		if($_POST['name'] == '')
+		{
+			$error['name'] = 'blank';
+		}
+		if($_POST['email'] == '')
+		{
+			$error['email'] = 'blank';
+		}
+		if(strlen($_POST['password'])< 4 )
+		{
+			$error['password'] = 'length';
+		}
+		if($_POST['password'] == '')
+		{
+			$error['password'] = 'blank';
+		}
+		// $fileName = $_FILES['image']['name'];
+		// if(!empty($fileName))
+		// {
+		// 	$ext = substr($fileName, -3);
+		// 	if($ext != 'jpg' && $ext != 'gif')
+		// 	{
+		// 		$error['image'] = 'type';
+		// 	}
+		// }
+	
+		//正常に入力されたら
+		// if(empty($error)) 
+		// {
+		// 	画像をアップロードする
+		// 	$image = date('YmdHis'). $_FILES['image']['name'];
+		// 	move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/'.$image);
+			
+		// 	$_SESSION['join'] = $_POST;
+		// 	$_SESSION['join']['image'] = $image;
+			
+		// 	画面遷移(画面観の移動)
+		// 	header('Location: pmain.php');
+		// 	exit();
+		// }
+		// 書き直し
+		// if($_REQEST['action'] == 'rewrite')
+		// {
+		// 	$_POST = $_SESSION ['join'];
+		// 	$error ['rewrite'] = true;
+		// }
+		// else
+		// {
+		// 	$_POST['name']='';
+		// 	$_POST['email']='';
+		// 	$_POST['password']='';
+
+
+		// 
+		// var_dump($error);
+		//POST送信が押されてデータが送られてきた時
+		if (empty($error))
+		{
+			//登録処理を行う
+			$sql = sprintf(
+			'INSERT INTO members SET name="%s", email="%s", password="%s", created="%s"',
+			mysqli_real_escape_string($db, $_POST['account']),
+			mysqli_real_escape_string($db, $_POST['email']),
+			mysqli_real_escape_string($db, sha1($_POST['password'])),
+			date('Y-m-d H:i:s')
+			);
+			mysqli_query($db, $sql) or die(mysqli_error($db));
+
+			//SESSION変数の破棄
+			unset($_SESSION['join']);
+
+			header('Location: ../top/top.html');
+			exit();
+		}
+	}
+
+		
+
+?>
 <!DOCTYPE html>
 <html>
 <style>
@@ -95,18 +184,31 @@ font-family: '[フォント名]', sans-serif;
 					<div id="jQueryBox">
 					    <div class="touroku">
 							<a name="touroku">
-								<form style="text-align:center">
+								<form style="text-align:center" action="" method="post" name="signup" enctype="multipart/form-data">
 									</br>
 									<dt>アカウント名</dt>
-									<dd><input type="text" name="account" class="span3"></dd>
+									<dd><input type="text" name="name" class="span3"></dd>
+									<?php if (isset($error['name']) && ($error['name'] == 'blank')): ?>
+									<p class="error">ニックネームを入力してください</p>
+									<?php endif; ?>
 
 									<dt>メールアドレス</dt>
-									<dd><input type="email" name="email" class="span3"></dd>
-
+									<dd><input type="email" name="email" class="span3" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES,'UTF-8'); ?>" ></dd>
+									<?php if ($error['email'] == 'blank'): ?>
+									<p class="error">メールアドレスを入力してください</p>
+									<?php endif; ?>
 									<dt>パスワード</dt>
-									<dd><input type="password" name="password" class="span3"></dd>
 
-									<a class="btn signup" input type="submit" value="Sign up">Sign up</a>
+									<dd><input type="password" name="password" class="span3" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8'); ?>"></dd>
+									<?php if ($error['password'] == 'blank'): ?>
+									<p class="error">パスワードを入力してください</p>
+									<?php endif; ?>
+									<?php if ($error['password'] == 'length'): ?>
+									<p class="error">パスワードは4文字以上で入力してください</p>
+									<?php endif; ?>
+
+
+									<a href="javascript:void(0)" onclick="document.signup.submit();";><div class="btn btn-signup" input type="submit" >Sign up</div></a>
 								</form>
 							</a>
 						</div>
